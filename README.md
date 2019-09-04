1ef0a59c12052f834517cff4773050c92307f241
KYGAS <45013496+KYGAS@users.noreply.github.com>
2019-09-04 03:30:51 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Stop-Guild-Quitting
+
+Command: "quitguild"
+
+After you use the command you can leave the guild once, after that, it will prevent you again until the next time you use the command.
---
+++ index.js
@@ -0,0 +1,28 @@
+module.exports = function StopGuildQuitting(dispatch) {
+	
+    const command = dispatch.command;
+	
+	let enabled = true;
+
+	command.add('quitguild', {
+		$default() { 
+			enabled = !enabled;
+			if(enabled){
+				command.message('You can quit your guild now. 10 seconds from now you will be prevented to leave the guild again until the .');
+				command.message('10 seconds from now you will be prevented to leave the guild again until the next time you use the command.');
+				setTimeout(function(){
+					enabled = false;
+				}, 10000);
+			}
+			else command.message('You are safe from leaving the guild.');
+    	}
+	});
+	
+	dispatch.hook("C_REQUEST_COOLTIME_TO_JOIN_GUILD", 'raw', () => {
+		if(enabled) return false;
+	});
+	
+	dispatch.hook("S_REQUEST_COOLTIME_TO_JOIN_GUILD", 'raw', () => {
+		if(enabled) return false;
+	});
+}
